e1da5b513479655c1cec9920cdd9d9105940361a
emrizkiem <mr.hobskoy@gmail.com>
2020-04-07 04:17:52 +0700

    Added spinner :kissing_heart:

--- app/src/main/java/dev/emrizkiem/covid19/ui/home/HomeFragment.kt
+++ app/src/main/java/dev/emrizkiem/covid19/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -23,6 +24,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
