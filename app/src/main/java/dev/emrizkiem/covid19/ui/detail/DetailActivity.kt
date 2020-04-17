18e0bf73ef4e8b9050ab2fa03f29e98e2ef0e584
emrizkiem <mr.hobskoy@gmail.com>
2020-04-17 16:44:23 +0700

    Update resource layout detail :kissing_heart:

--- app/src/main/java/dev/emrizkiem/covid19/ui/detail/DetailActivity.kt
+++ app/src/main/java/dev/emrizkiem/covid19/ui/detail/DetailActivity.kt
@@ -3,11 +3,36 @@ package dev.emrizkiem.covid19.ui.detail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dev.emrizkiem.covid19.R
+import dev.emrizkiem.covid19.data.model.home.CovidDetail
+import dev.emrizkiem.covid19.ui.maps.MapsFragment
+import dev.emrizkiem.covid19.util.CaseType
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
+
+    private var mapsFragment: MapsFragment? = null
+
+    private val caseType by lazy {
+        intent.getIntExtra(CASE_TYPE, CaseType.CONFIRMED)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        fab_back.setOnClickListener { onBackPressed() }
+        attachMaps()
+    }
+
+    private fun attachMaps() {
+        //mapsFragment = MapsFragment.newInstance(ArrayList(data), caseType)
+        mapsFragment?.let {
+            supportFragmentManager.beginTransaction().replace(R.id.content_frame, it)
+                .commit()
+        }
+    }
+
+    companion object {
+        private const val CASE_TYPE = "case_type"
     }
 }
